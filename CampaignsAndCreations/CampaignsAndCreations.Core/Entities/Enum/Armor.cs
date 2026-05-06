d0559e059d92375a96dbb92f609f77f2bf4121f5
KateVerleyen <roel.verleyen@telenet.be>
2026-05-07 00:00:42 +0200

    Add armor enum

---
+++ CampaignsAndCreations/CampaignsAndCreations.Core/Entities/Enum/Armor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignsAndCreations.Core.Entities.Enum
+{
+    public enum Armor
+    {
+        // Light Armor
+        Padded,
+        Leather,
+        StuddedLeather,
+
+        // Medium Armor
+        Hide,
+        ChainShirt,
+        ScaleMail,
+        Breastplate,
+        HalfPlate,
+
+        // Heavy Armor
+        RingMail,
+        ChainMail,
+        Splint,
+        Plate,
+
+        // Shields
+        Shield,
+
+        // Composite Categories
+        LightArmor,
+        MediumArmor,
+        HeavyArmor
+    }
+}
