7170fff1bbb8f1fb857d2e43766bb1b24ddd9f84
KateVerleyen <roel.verleyen@telenet.be>
2026-05-06 23:50:52 +0200

    Add skill entity

---
+++ CampaignsAndCreations/CampaignsAndCreations.Core/Entities/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignsAndCreations.Core.Entities
+{
+    public abstract class Skill
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Skill(string name, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+        }
+    }
+}
