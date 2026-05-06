8ff2abe27ba354791f8adf85b0ba9e0298eabd4f
KateVerleyen <roel.verleyen@telenet.be>
2026-05-06 23:45:42 +0200

    Add feat entity

---
+++ CampaignsAndCreations/CampaignsAndCreations.Core/Entities/Feat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignsAndCreations.Core.Entities
+{
+    public abstract class Feat
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Feat(string name, string description) 
+        { 
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+        }
+    }
+}
