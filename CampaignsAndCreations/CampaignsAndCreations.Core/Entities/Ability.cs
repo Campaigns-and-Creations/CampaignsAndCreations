022b4a36e17f08376aedbc384d4754d6b06e1b31
KateVerleyen <roel.verleyen@telenet.be>
2026-05-06 23:30:38 +0200

    Update ability type

--- CampaignsAndCreations/CampaignsAndCreations.Core/Entities/Ability.cs
+++ CampaignsAndCreations/CampaignsAndCreations.Core/Entities/Ability.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Beyond_Sheet.Core.Entities
 {
-    public class Ability
+    public abstract class Ability
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
